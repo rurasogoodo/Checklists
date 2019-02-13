137133b2c6a9a3d13ce75ec0a6c068958fef72cd
Nick_Romanenko <ruras.dalgard@gmail.com>
2019-02-13 15:41:52 +0200

    создали таблицу

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -19,19 +19,50 @@ class ChecklistViewController: UITableViewController {
     
     // устанавливаем количество ячеек для таблицы
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 10
     }
     
+    // Указываем прототип ячейки, который будет дублироваться для таблицы
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // создаем ячейку
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
         
+        // находим у ячейки Лейбл по Тегом "1000"
         let label = cell.viewWithTag(1000) as! UILabel
         
-        
+        // указываем, что будет в Лейбле под тегом "1000", если его местоположение будет в нужно строке
+        if indexPath.row == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row == 1 {
+            label.text = "Brush the teeth"
+        } else if indexPath.row == 2 {
+            label.text = "Learn iOS development"
+        } else if indexPath.row == 3 {
+            label.text = "Soccer practice"
+        } else if indexPath.row == 4 {
+            label.text = "Eat ice cream"
+        }
         return cell
     }
     
+    //MARK: - Table View Delegate
     
+    // указываем, что делать, если нажали на какую-то ячейку в таблице
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // ставим/убираем "галочку" на выделенной ячейке
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        
+        // снимаем "выделение" с этой нажатой ячейки
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
 }
 
