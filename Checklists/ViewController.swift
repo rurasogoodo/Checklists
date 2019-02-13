92d21155b73b2ba8acb86a60aa2f64f6eaae571b
Nick_Romanenko <ruras.dalgard@gmail.com>
2019-02-13 11:57:48 +0200

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Nick_Romanenko on 2/13/19.
+//  Copyright © 2019 Nick_Romanenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
